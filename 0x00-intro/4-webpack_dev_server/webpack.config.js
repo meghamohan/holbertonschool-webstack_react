999e263adb3f2d869688accb64990dbd6a3de98b
meghamohan <meghamohan1989@gmail.com>
2018-09-12 22:45:06 +0000

    adding task 5

---
+++ 0x00-intro/4-webpack_dev_server/webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({title: 'Holberton School webpack'})
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: 5000
+  }
+};
