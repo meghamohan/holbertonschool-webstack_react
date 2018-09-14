7831cafe51df05967214b740a3992158eacd48dc
meghamohan <meghamohan1989@gmail.com>
2018-09-14 05:51:09 +0000

    adding task11

---
+++ 0x00-intro/10-react_component_prop/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Title from './Title.jsx';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <Title name = "Holberton School"/>
+      </div>);
+  }
+}
