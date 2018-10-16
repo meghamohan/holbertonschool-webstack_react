67e13beb1f8ad9cc4f9357a29d980da38ab2bdfd
meghamohan <meghamohan1989@gmail.com>
2018-10-16 00:44:53 +0000

    initial commit of task5

---
+++ 0x01-state/5-counter/src/components/Counter.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.initValue
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.state.value}
+                <button onClick={(e) => this.handleClick(e)}>
+                    New Value
+                </button>
+            </div>
+        );
+    }
+    handleClick() {
+        this.setState({value: this.state.value + 1});
+    }
+}
+
+Counter.propTypes = {
+    value: PropTypes.number.isRequired
+};
+
+Counter.defaultProps =  {
+    value: 0
+};
