0aff2b919435e92428b4a8b61e90841ae31de2d4
meghamohan <meghamohan1989@gmail.com>
2018-10-28 20:13:38 +0000

    initial commmit

---
+++ 0x01-state/9-todolist/src/components/Item.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Item extends React.Component {
+    render() {
+        return (<li>{this.props.value}</li>);
+    }
+}
+
+Item.propTypes = {value: PropTypes.string};
